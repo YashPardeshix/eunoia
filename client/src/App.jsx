1fafcffd14f4456826ebd51cf16928fc0e3f6b92
Yash Pardeshi <ashpardeshi07@gmail.com>
2025-11-04 02:42:56 +0530

    feat: Increment 3 - Local Storage Hook and UI Shell

---
+++ client/src/App.jsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-blue-500 text-white p-10 rounded-2xl text-3xl">
+      Tailwind 4 is now working perfectly 🎉
+    </div>
+  );
+}
